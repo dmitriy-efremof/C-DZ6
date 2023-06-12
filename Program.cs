9c3c34993d51d6b138b6aed810fe8c6adbd5582c
Dmitriy <132077746+dmitriy-efremof@users.noreply.github.com>
2023-06-12 19:37:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -27,19 +27,19 @@
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-Console.WriteLine("Введите значение b1:");
-int b1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение b1:");
+// int b1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите значение k1:");
-int k1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k1:");
+// int k1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите значение b2:");
-int b2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение b2:");
+// int b2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите значение k2:");
-int k2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите значение k2:");
+// int k2 = Convert.ToInt32(Console.ReadLine());
 
-double x = -(b1 - b2) / (k1 - k2);
-double y = k1 * x + b1;
+// double x = -(b1 - b2) / (k1 - k2);
+// double y = k1 * x + b1;
 
-Console.WriteLine($"Точка пересечения двух прямых: ({x}:{y})");
+// Console.WriteLine($"Точка пересечения двух прямых: ({x}:{y})");
